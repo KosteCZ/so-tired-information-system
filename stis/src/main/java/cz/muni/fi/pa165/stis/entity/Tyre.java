d1b587b23fd6257c06226f37846497be1523d8b0
xkoscak@gmail.com <xkoscak@gmail.com@8cdafaee-e879-d393-fde2-c184d527451b>
2012-10-12 22:02:38 +0000

    Tyre. Functional version 1.

--- stis/src/main/java/cz/muni/fi/pa165/stis/entity/Tyre.java
+++ stis/src/main/java/cz/muni/fi/pa165/stis/entity/Tyre.java
@@ -1,16 +1,27 @@
 package cz.muni.fi.pa165.stis.entity;
 
-/**
- *
- * @author xkoscak
- */
-public class Tyre {
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+/**
+ * This Tyre entity contains data about tyres offered by our company.
+ *
+ * @author Jan Koščák (xkoscak@gmail.com)
+ */
+@Entity
+public class Tyre implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String type;
     private String name;
-    private int size;
-    private int price;
+    private Double diameter;
+    private BigDecimal price;
     private String vendor;
 
     public Long getId() {
@@ -37,19 +48,19 @@ public class Tyre {
         this.name = name;
     }
 
-    public int getSize() {
-        return size;
+    public Double getDiameter() {
+        return diameter;
     }
 
-    public void setSize(int size) {
-        this.size = size;
+    public void setDiameter(Double diameter) {
+        this.diameter = diameter;
     }
 
-    public int getPrice() {
+    public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(int price) {
+    public void setPrice(BigDecimal price) {
         this.price = price;
     }
 
@@ -59,5 +70,31 @@ public class Tyre {
 
     public void setVendor(String vendor) {
         this.vendor = vendor;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Tyre other = (Tyre) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Tyre{" + "id=" + id + ", type=" + type + ", name=" + name + ", diameter=" + diameter + ", price=" + price + ", vendor=" + vendor + '}';
     }
 }
