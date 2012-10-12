d2bf3c682752355ff1db3633cfd7247236088039
xkoscak@gmail.com <xkoscak@gmail.com@8cdafaee-e879-d393-fde2-c184d527451b>
2012-10-12 23:00:56 +0000

    TyreDAO and TyreDAOImpl. Both functional versions 1.

--- trunk/stis/src/main/java/cz/muni/fi/pa165/stis/dao/impl/TyreDAOImpl.java
+++ trunk/stis/src/main/java/cz/muni/fi/pa165/stis/dao/impl/TyreDAOImpl.java
@@ -5,10 +5,11 @@ import cz.muni.fi.pa165.stis.entity.Tyre;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
 
 /**
  *
- * @author xkoscak
+ * @author Jan Koščák (xkoscak@gmail.com)
  */
 public class TyreDAOImpl implements TyreDAO {
 
@@ -32,26 +33,65 @@ public class TyreDAOImpl implements TyreDAO {
 
     @Override
     public Tyre get(Long id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (id == null) {
+            throw new IllegalArgumentException("id is null");
+        }
+        EntityManager em = emf.createEntityManager();
+        return em.find(Tyre.class, id);
     }
 
     @Override
     public void update(Tyre tyre) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (tyre == null) {
+            throw new IllegalArgumentException("tyre is null");
+        }
+        if (tyre.getId() == null) {
+            throw new IllegalArgumentException("tyre id is null");
+        }
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(tyre);
+        em.refresh(tyre);
+        em.getTransaction().commit();
+        em.close();
     }
 
     @Override
     public void remove(Tyre tyre) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (tyre == null) {
+            throw new IllegalArgumentException("order is null");
+        }
+        if (tyre.getId() == null) {
+            throw new IllegalArgumentException("tyre id is null");
+        }
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        Tyre toRemove = em.find(Tyre.class, tyre.getId());
+        if (toRemove == null) {
+            throw new IllegalArgumentException("tyre does not exist");
+        }
+        em.remove(toRemove);
+        em.getTransaction().commit();
+        em.close();
     }
 
     @Override
     public List<Tyre> findAll() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<Tyre> tq = em.createQuery("FROM Tyre", Tyre.class);
+        em.close();
+        return tq.getResultList();
     }
 
     @Override
-    public Tyre findByName(String name) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public List<Tyre> findByName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("name is null");
+        }
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<Tyre> tq = em.createQuery("SELECT t FROM Tyre t WHERE t.name LIKE :name", Tyre.class);
+        tq.setParameter("name", name);
+        em.close();
+        return tq.getResultList();
     }
 }
