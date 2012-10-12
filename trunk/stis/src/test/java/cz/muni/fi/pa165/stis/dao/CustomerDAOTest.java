b77bc226dc2851bfc4870a35dae9de632cd56291
dusan.svancara@gmail.com <dusan.svancara@gmail.com@8cdafaee-e879-d393-fde2-c184d527451b>
2012-10-12 12:23:13 +0000

    merge stis-temp + zakomentovany test -- cca ako by mal vyzerat + nejake opravy

---
+++ trunk/stis/src/test/java/cz/muni/fi/pa165/stis/dao/CustomerDAOTest.java
@@ -0,0 +1,141 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.stis.dao;
+
+import cz.muni.fi.pa165.stis.dao.impl.CustomerDAOImpl;
+import cz.muni.fi.pa165.stis.entity.Customer;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author xsvanca2
+ */
+public class CustomerDAOTest {
+    
+    public CustomerDAOTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of create method, of class CustomerDAO.
+     */
+    @Test
+    public void testCreate() {
+//        System.out.println("create");
+//        Customer customer = null;
+//        CustomerDAO instance = new CustomerDAOImpl();
+//        instance.create(customer);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of get method, of class CustomerDAO.
+     */
+    @Test
+    public void testGet() {
+//        System.out.println("get");
+//        Long id = null;
+//        CustomerDAO instance = new CustomerDAOImpl();
+//        Customer expResult = null;
+//        Customer result = instance.get(id);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of update method, of class CustomerDAO.
+     */
+    @Test
+    public void testUpdate() {
+//        System.out.println("update");
+//        Customer customer = null;
+//        CustomerDAO instance = new CustomerDAOImpl();
+//        instance.update(customer);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of remove method, of class CustomerDAO.
+     */
+    @Test
+    public void testRemove() {
+//        System.out.println("remove");
+//        Customer customer = null;
+//        CustomerDAO instance = new CustomerDAOImpl();
+//        instance.remove(customer);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of findAll method, of class CustomerDAO.
+     */
+    @Test
+    public void testFindAll() {
+//        System.out.println("findAll");
+//        CustomerDAO instance = new CustomerDAOImpl();
+//        List expResult = null;
+//        List result = instance.findAll();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of findByName method, of class CustomerDAO.
+     */
+    @Test
+    public void testFindByName_String() {
+//        System.out.println("findByName");
+//        String lastName = "";
+//        CustomerDAO instance = new CustomerDAOImpl();
+//        List expResult = null;
+//        List result = instance.findByName(lastName);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of findByName method, of class CustomerDAO.
+     */
+    @Test
+    public void testFindByName_String_String() {
+//        System.out.println("findByName");
+//        String lastName = "";
+//        String firstName = "";
+//        CustomerDAO instance = new CustomerDAOImpl();
+//        List expResult = null;
+//        List result = instance.findByName(lastName, firstName);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+    
+}
