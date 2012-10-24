0c9e28df6642abfda02e3d4fdea767bece94034a
dusan.svancara@gmail.com <dusan.svancara@gmail.com@8cdafaee-e879-d393-fde2-c184d527451b>
2012-10-24 13:39:58 +0000

    - ExtraServiceService a jeho Implementacia - priklad

---
+++ stis/src/main/java/cz/muni/fi/pa165/stis/service/impl/ExtraServiceServiceImpl.java
@@ -0,0 +1,51 @@
+package cz.muni.fi.pa165.stis.service.impl;
+
+import cz.muni.fi.pa165.stis.dao.ExtraServiceDAO;
+import cz.muni.fi.pa165.stis.entity.ExtraService;
+import cz.muni.fi.pa165.stis.service.ExtraServiceService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author Dusan Svancara
+ */
+@Service
+public class ExtraServiceServiceImpl implements ExtraServiceService {
+    
+    @Autowired
+    private ExtraServiceDAO dao;
+
+    @Transactional
+    public void create(ExtraService extraService) {
+        dao.create(extraService);
+    }
+
+    @Transactional(readOnly = true)
+    public ExtraService get(Long id) {
+        return dao.get(id);
+    }
+
+    @Transactional
+    public void update(ExtraService extraService) {
+        dao.update(extraService);
+    }
+
+    @Transactional
+    public void remove(ExtraService extraService) {
+        dao.remove(extraService);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ExtraService> findAll() {
+        return dao.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public List<ExtraService> findByName(String name) {
+        return dao.findByName(name);
+    }
+    
+}
