b450b825f481cb241fb34802c9236ebf1e14501a
michalxo@gmail.com <michalxo@gmail.com@8cdafaee-e879-d393-fde2-c184d527451b>
2012-11-06 08:28:32 +0000

    Added empty test classes for TyreServiceImpl & TyreDAOImpl. (branch commit test too)

---
+++ StisEJB-ejb/src/test/java/cz/muni/fi/pa165/stis/dao/impl/TyreDAOImplTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.stis.dao.impl;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author michalxo
+ */
+public class TyreDAOImplTest {
+    
+    /*
+     * "classic test - junit no need for mockito"  
+     */
+    
+    public TyreDAOImplTest() {
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+    }
+
+    @Test
+    public void testGet() throws Exception {
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+    }
+
+    @Test
+    public void testRemove() throws Exception {
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+    }
+
+    @Test
+    public void testFindByName() throws Exception {
+    }
+}
---
+++ StisEJB-ejb/src/test/java/cz/muni/fi/pa165/stis/service/impl/TyreServiceImplTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.stis.service.impl;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author michalxo
+ */
+public class TyreServiceImplTest {
+    
+    /*
+     * Test using mockito (see spring testing)
+     */
+    
+    public TyreServiceImplTest() {
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+    }
+
+    @Test
+    public void testGet() throws Exception {
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+    }
+
+    @Test
+    public void testRemove() throws Exception {
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+    }
+
+    @Test
+    public void testFindByName() throws Exception {
+    }
+}
