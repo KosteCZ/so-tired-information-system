abe649d563bea2631b12eb5a148e74112642f04b
michalxo@gmail.com <michalxo@gmail.com@8cdafaee-e879-d393-fde2-c184d527451b>
2012-12-09 09:07:15 +0000

    REST Client - spring

--- stis/stis-rest-client/src/main/java/cz/muni/fi/pa165/stis/rest/client/ExtraServiceClientActionBean.java
+++ stis/stis-rest-client/src/main/java/cz/muni/fi/pa165/stis/rest/client/ExtraServiceClientActionBean.java
@@ -1,9 +1,6 @@
 package cz.muni.fi.pa165.stis.rest.client;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import cz.muni.fi.pa165.stis.dto.ExtraServiceTO;
-import org.springframework.web.client.RestTemplate;
-import java.util.List;
 import net.sourceforge.stripes.action.ActionBean;
 import net.sourceforge.stripes.action.ActionBeanContext;
 import net.sourceforge.stripes.action.Before;
@@ -13,35 +10,40 @@ import net.sourceforge.stripes.action.RedirectResolution;
 import net.sourceforge.stripes.action.Resolution;
 import net.sourceforge.stripes.action.UrlBinding;
 import net.sourceforge.stripes.controller.LifecycleStage;
+import net.sourceforge.stripes.integration.spring.SpringBean;
 import net.sourceforge.stripes.validation.Validate;
 import net.sourceforge.stripes.validation.ValidateNestedProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 /**
  *
  * @author Michal Toth
  */
 
+@Component
 @UrlBinding("/extraservice/{$event}/")
 public class ExtraServiceClientActionBean implements ActionBean {
 
-    ObjectMapper mapper = new ObjectMapper();
     static String HOST = "localhost";
     static int PORT = 8080;
     static String webapp = "pa165/rest";
-    String url = "http://" + HOST + ":" + PORT + "/" + webapp + "/extraservices";
+    final String url = "http://" + HOST + ":" + PORT + "/" + webapp + "/extraservices";
     final static Logger logger = LoggerFactory.getLogger(ExtraServiceClientActionBean.class);
+    
     private ActionBeanContext context;
-    private RestTemplate rt = new RestTemplate();
+
+    @SpringBean
+    RestTemplate restTemplate;// = new RestTemplate();
    
     @ValidateNestedProperties(value = {
         @Validate(on = {"create", "save"}, field = "name", required = true),
         @Validate(on = {"create", "save"}, field = "price", required = true, minvalue = 1)
     })
-    private ExtraServiceTO extraService;
-    private List<ExtraServiceTO> results;
+    private ExtraServiceTO extraService;    
 
     @DefaultHandler
     public Resolution list() {
@@ -56,7 +58,7 @@ public class ExtraServiceClientActionBean implements ActionBean {
 
     public Resolution save() {
         logger.debug("save() {}", extraService);
-        rt.put(url + "/{id}", extraService, extraService.getId());
+        restTemplate.put(url + "/{id}", extraService, extraService.getId());
         System.out.println(extraService.toString());
         return new RedirectResolution(this.getClass(), "list.jsp");
     }
@@ -65,17 +67,15 @@ public class ExtraServiceClientActionBean implements ActionBean {
     public void loadESFromDatabase() {
         String id = context.getRequest().getParameter("extraService.id");        
         if (id != null) {
-            extraService = rt.getForObject(url + "/{id}", ExtraServiceTO.class, id);
-        }
-
-        System.out.println("DB obj -> EXTRA SERVICE=" + extraService);
+            extraService = restTemplate.getForObject(url + "/{id}", ExtraServiceTO.class, id);
+        }        
     }
 
     public Resolution delete() {
 
         logger.debug("delete({})", extraService);
         try {
-            rt.delete(url + "/{id}", extraService.getId());
+            restTemplate.delete(url + "/{id}", extraService.getId());
         } catch (RestClientException e) {
             System.err.println("Exception has been thrown " + e.getStackTrace());
         }
@@ -89,13 +89,13 @@ public class ExtraServiceClientActionBean implements ActionBean {
 
     public Resolution create() {
         logger.debug("create() {}", extraService);
-        rt.postForObject(url + "/", extraService, ExtraServiceTO.class);
+        restTemplate.postForObject(url + "/", extraService, ExtraServiceTO.class);
         return new RedirectResolution(this.getClass(), "list");
     }
 
     public ExtraServiceTO[] getAllExtraServices() {
         logger.debug("getAllExtraService()");
-        ExtraServiceTO[] ess = rt.getForObject(url + "/", ExtraServiceTO[].class);
+        ExtraServiceTO[] ess = restTemplate.getForObject(url + "/", ExtraServiceTO[].class);
         return ess;
     }
 
