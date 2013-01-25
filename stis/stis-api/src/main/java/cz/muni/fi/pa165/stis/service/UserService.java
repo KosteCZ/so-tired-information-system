3fa456507964e7332e5ffa30ec94af2fa2b4ab58
michalxo@gmail.com <michalxo@gmail.com@8cdafaee-e879-d393-fde2-c184d527451b>
2013-01-25 14:53:36 +0000

    Minor bugfix, test and javadoc added.

--- stis/stis-api/src/main/java/cz/muni/fi/pa165/stis/service/UserService.java
+++ stis/stis-api/src/main/java/cz/muni/fi/pa165/stis/service/UserService.java
@@ -10,19 +10,64 @@ import cz.muni.fi.pa165.stis.dto.UserTO;
  */
 public interface UserService {
 
+    /**
+     * Creates new UserTO object
+     * 
+     * @param user object to be created
+     */
     void create(UserTO user);
 
+    /**
+     * Removes userTO object.
+     * 
+     * @param user object to be removed
+     */
     void remove(UserTO user);
 
+    /**
+     *  Update userTO object
+     * 
+     * @param user object to be updated
+     */
     void update(UserTO user);
 
+    /**
+     * Returns UserTO object with given ID
+     * 
+     * @param id return UserTO with given ID
+     * @return UserTO object
+     */
     UserTO get(Long id);
 
+    /**
+     * Searches and returns UserTO entity with
+     * provided username.
+     * 
+     * @param username find UserTO with given username
+     * @return UserTO with given username
+     */
     UserTO getByUsername(String username);
 
+    /**
+     * Check for available/free username.
+     * Username is unique parameter in User entity.
+     * 
+     * @return true if username is free for use
+     */
     boolean availableUsername(String username);
 
+    /**
+     * Check whether user is admin or not
+     * 
+     * @param user to be checked for admin yes/no
+     * @return true if user is admin
+     */
     boolean isAdmin(UserTO user);
 
+    /**
+     * Make user an admin
+     * 
+     * @param user to have change role to admin
+     */
     void makeAdmin(UserTO user);
 }
