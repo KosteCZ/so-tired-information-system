df0bf232fdc10a2021ae9d986eaab0db398dd1b4
michalxo@gmail.com <michalxo@gmail.com@8cdafaee-e879-d393-fde2-c184d527451b>
2013-01-26 19:16:19 +0000

    Error Handler working with localization.

--- trunk/stis/stis-web/src/main/java/cz/muni/fi/pa165/stis/web/ExceptionHandler.java
+++ trunk/stis/stis-web/src/main/java/cz/muni/fi/pa165/stis/web/ExceptionHandler.java
@@ -1,6 +1,5 @@
 package cz.muni.fi.pa165.stis.web;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import net.sourceforge.stripes.action.ForwardResolution;
@@ -8,7 +7,6 @@ import net.sourceforge.stripes.action.Resolution;
 import net.sourceforge.stripes.exception.DefaultExceptionHandler;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.client.HttpServerErrorException;
 
 /**
  * ExceptionHandler class takes care of exceptions
@@ -21,21 +19,7 @@ import org.springframework.web.client.HttpServerErrorException;
 public class ExceptionHandler extends DefaultExceptionHandler {
         
     final static org.slf4j.Logger logger = LoggerFactory.getLogger(ExceptionHandler.class);
-    private String rootCause;
-      
-    public Resolution handleServletException(ServletException exc, HttpServletRequest request, HttpServletResponse response) {
-        rootCause = this.getCause(exc).toString();
-        logger.error("handleServletException() {}", exc,  " ROOT CAUSE=", rootCause);          
-        return new ForwardResolution(ErrorHandlerActionBean.class, "error").addParameter("exception", exc.toString()).addParameter("rootCause", rootCause);                
-    }
-        
-    
-    public Resolution handleServerErrorException(HttpServerErrorException exc, HttpServletRequest request, HttpServletResponse response) {
-        rootCause = this.getCause(exc).toString();
-        logger.error("handleServerErrorException({})", exc, " ROOT CAUSE=", rootCause);    
-        return new ForwardResolution(ErrorHandlerActionBean.class, "error").addParameter("exception", exc).addParameter("rootCause", rootCause);
-    }
-    
+    private String rootCause;        
     
     public Resolution handleGeneric(Exception exc, HttpServletRequest request, HttpServletResponse response) {
         // general exception handling
